27d6fe89e826038de6bc2be4062ae34b407633a8
Wang Sheng Fan <wang2147@flinders.edu.au>
2023-08-21 21:03:18 +0930

    Practical 2 first commit

---
+++ W03/scripts/inc-items.js
@@ -0,0 +1,30 @@
+const product = [
+  {
+    itemName: "Apple",
+    cost: "$5.00",
+    healthy: true,
+    categorty: "fruit",
+    location: "Aisle 2",
+  },
+  {
+    itemName: "Chocolate",
+    cost: "$2.50",
+    healthy: false,
+    categorty: "confectionary",
+    location: "Aisle 7",
+  },
+  {
+    itemName: "Pizza",
+    cost: "$15.00",
+    healthy: false,
+    categorty: "take out",
+    location: "Aisle 15",
+  },
+  {
+    itemName: "Milk",
+    cost: "$4.00",
+    healthy: true,
+    categorty: "dairy",
+    location: "Aisle 12",
+  },
+];
