fd1880f00273fc64519f8c9b5eb6e83682eff992
Wang Shengfan <wang2147@flinders.edu.au>
2023-09-08 01:46:45 +0930

    Practical 3 Checkpoint 1

--- prac-3/checkpoint-1/index.php
+++ prac-3/checkpoint-1/index.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Wang Shengfan" />
     <title>Practical 3: Current tasks</title>
-    <meta charset="UTF-8" />
-    <meta name="author" content="<replace with your name>" />
     <!-- <script src="scripts/script.js" defer></script> -->
 </head>
+
 <body>
     <!-- <?php require_once "inc/menu.inc.php"; ?> -->
     <h1>Current</h1>
     <?php
     require_once "inc/dbconn.inc.php";
 
-    // Add your code here
+    $sql = "SELECT id, name FROM Task WHERE completed=0;";
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) >= 1) {
+            //  Parse the results and generate the HTML necessary to display them on the page.
+            // Access each of the rows by using the mysqli_fetch_assoc function
+            echo "<ul>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<li>" . $row["name"] . "</li>";
+            }
+            echo "</ul>";
+        }
+        mysqli_free_result($result);
+    }
+
+    mysqli_close($conn);
 
     ?>
 </body>
+
 </html>
