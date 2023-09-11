c44673534971426723369da3164b81a1496f17ce
Wang Shengfan <wang2147@flinders.edu.au>
2023-09-11 10:57:26 +0930

    cleanups

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
 "use strict";
 // Function to toggle the dropdown
 function toggleDropdown() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
 document.querySelector(".account").addEventListener("click", toggleDropdown);
 
 window.addEventListener("click", function (event) {
-  if (!event.target.matches(".account")) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
+    if (!event.target.matches(".account")) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
     }
-  }
 });
